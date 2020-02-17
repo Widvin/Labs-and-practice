e5d52320e95b8b83835e9087952096ecd81e3234
Widvin <55740261+Widvin@users.noreply.github.com>
2020-02-17 18:51:45 +0300

    Add files via upload

--- expenses.java
+++ expenses.java
@@ -1,40 +1,51 @@
 package by.gsu.pms;
 
 public class expenses {
-    int Rate = 25000;
+    private int Rate = 25000;
     String Account;
     float Expenses;
     int NumberOfDays;
 
 
     public static class TripExpenses(String _account, float _expenses, int _numberOfDays){
-        Account = _account;
-        Expenses = _expenses;
-        NumberOfDays = _numberOfDays;
+        this.Account = _account;
+        this.Expenses = _expenses;
+        this.NumberOfDays = _numberOfDays;
+    }
+    public Cheque() {
+        this.Account = "Name";
+        this.Expenses = 0;
+        this.NumberOfDays = 1;
     }
 
     public String getAccount() {
+
         return Account;
     }
 
     public void setAccount(String acc) {
+
         Account = acc;
     }
 
-    public float getExpenses() {
+    public float getExpenses()
+    {
         return Expenses;
     }
 
-    public void setExpenses(float exp) {
-        Expenses = exp;
+    public void setExpenses(float exp)
+    {
+        this.Expenses = exp;
     }
 
-    public int getNumberOfDays() {
+    public int getNumberOfDays()
+    {
         return NumberOfDays;
     }
 
     public void setNumberOfDays(int days) {
-        NumberOfDays = days;
+
+        this.NumberOfDays = days;
     }
 
         public float get_total() {
@@ -44,4 +55,21 @@ public class expenses {
                 return Rate * NumberOfDays;
             }
         }
+
+    public void show(){
+        System.out.println(" - - - - - - - ");
+        System.out.println("Командировочные выплаты = " + Rate);
+        System.out.println("Чек = " + Account);
+        System.out.println("Расход на транспорт = " + Expenses);
+        System.out.println("Число дней = " + NumberOfDays);
+        System.out.println("Сумма = " + get_total());
+    }
+
+    public String to_string(){
+        return Rate +";"+ Account+";"+ Expenses";"+NumberOfDays+";"+get_total();
+    }
+}
+
+
+
     }
