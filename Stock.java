8bc125784cc91f932e8d7477548063d6a6b0f756
Widvin <55740261+Widvin@users.noreply.github.com>
2020-02-21 20:03:50 +0300

    Add files via upload

---
+++ Stock.java
@@ -0,0 +1,27 @@
+package by.gsu.pms;
+
+
+class Stock {
+    private String name;
+    private int count;
+    private boolean availability;
+
+    public Stock(String name, int count, boolean availability) {
+        this.count = count;
+        this.name = name;
+        this.availability = availability;
+    }
+
+      public String getname() {
+        return name;
+    }
+
+
+    public int getcount() {
+        return count;
+    }
+
+    public boolean availability() {
+        return availability;
+    }
+}
